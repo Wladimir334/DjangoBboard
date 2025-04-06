a99a6ceee0dcc1aee80677f916c87bcb37f1aa8d
Admin <Wladimir334@github.com>
2025-04-06 21:32:13 +0300

    HW060425

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from blog.views import index, about, add_post
+
+app_name = 'blog'
+urlpatterns = [
+    path('about', about, name='about'),
+    path('post/', add_post, name='add_post'),
+    path('', index, name='index'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+
+    context = {'title': "Главная страница", "posts":posts}
+    return render(request, template_name='blog/index.html', context=context)
+
+def about(request):
+    context = {'title': "О сайте"}
+    return render(request, template_name='blog/about.html', context=context)
+
+def add_post(request):
+    post_form = PostForm()
+    context = {'form': post_form}
+    return render(request, template_name='blog/post_add.html', context=context)
